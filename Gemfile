76dfb18cfce720f022f74c273bf3909d0249117c
Issue <zhushisong@skio.com>
2017-09-19 17:29:31 +0800

    cap

--- Gemfile
+++ Gemfile
@@ -31,6 +31,13 @@ group :development, :test do
 end
 
 group :development do
+  gem 'capistrano', '~> 3.3.0'
+  gem 'capistrano-rvm'
+  gem 'capistrano-rails-console'
+  gem 'capistrano-bundler', '~> 1.1.2'
+  gem 'capistrano-rails', '~> 1.1'
+  gem 'capistrano3-puma', require: false
+  
   gem 'listen', '>= 3.0.5', '< 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'react_api'
+set :repo_url, 'git@github.com:xiaohesong/react_api.git'
+
+set :user, 'xiaozhu'
+set :scm, :git
+
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/www/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/react-api-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
